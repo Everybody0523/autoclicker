3229a25fe2e3d33d0ae6e7872a68826b76d0d545
Neumann Hon <neumannkachun.hon@mail.utoronto.ca>
2016-12-31 20:35:43 -0500

    Rebuilt the entire thing using GUI Builder. Also, added a way to have delays between clicks. Did the front end of a thing that would allow scripts to be written, but for now that part remains non-functional.

--- src/autoclicker/Autoclicker.java
+++ src/autoclicker/Autoclicker.java
@@ -11,37 +11,120 @@ import javax.swing.JFrame;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.WindowConstants;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.SwingConstants;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JList;
 
 public class Autoclicker {
 	private final JFrame jframe;
-	private final JTextField numberOfClicks;
-	private final JButton beginClicking;
-
+	private JPanel inputPanel;
+	private JTextField delayTime;
+	private JTextField numClicks;
+	private JButton btnNewButton;
+	private JList list;
+	private JLabel lblNumberOfClicks;
+	private JLabel lblDelay;
+	private JButton btnRunScript;
+	
+	/**
+	 * Instantiate an AutoClicker, creating the necessary Swing objects within it.
+	 */
 	public Autoclicker() {
-		this.jframe = new JFrame();
-		this.numberOfClicks = new JTextField();
-		this.beginClicking = new JButton("Click Away!");
-		beginClicking.addActionListener(new ActionListener(){
+		//Build the frame and panel where all the inputs are.
+		this.jframe = new JFrame("Autoclicker");
+		this.inputPanel = new JPanel();
+		Container content = this.jframe.getContentPane();
+		content.add(this.inputPanel);
+		GridBagLayout gbl_inputPanel = new GridBagLayout();
+		gbl_inputPanel.columnWidths = new int[]{90, 90, 0, 0};
+		gbl_inputPanel.rowHeights = new int[]{37, 0, 0, 0, 0};
+		gbl_inputPanel.columnWeights = new double[]{0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_inputPanel.rowWeights = new double[]{1.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		inputPanel.setLayout(gbl_inputPanel);
+		
+		//Set up the JTextField where the amount of clicks to be performed will be executed is to be declared
+		//by the user
+		this.numClicks = new JTextField();
+		GridBagConstraints gbc_numClicks = new GridBagConstraints();
+		gbc_numClicks.insets = new Insets(0, 0, 5, 5);
+		gbc_numClicks.gridx = 0;
+		gbc_numClicks.gridy = 0;
+		this.inputPanel.add(this.numClicks, gbc_numClicks);
+		this.numClicks.setColumns(10);
+		
+		//Set up the JTextField where the delay between each click is to be declared by the user.
+		this.delayTime = new JTextField();
+		GridBagConstraints gbc_delayTime = new GridBagConstraints();
+		gbc_delayTime.anchor = GridBagConstraints.EAST;
+		gbc_delayTime.insets = new Insets(0, 0, 5, 5);
+		gbc_delayTime.gridx = 1;
+		gbc_delayTime.gridy = 0;
+		this.inputPanel.add(this.delayTime, gbc_delayTime);
+		this.delayTime.setColumns(10);
+		
+		//Set up the list of scripts.
+		this.list = new JList();
+		GridBagConstraints gbc_list = new GridBagConstraints();
+		gbc_list.gridheight = 4;
+		gbc_list.fill = GridBagConstraints.BOTH;
+		gbc_list.gridx = 2;
+		gbc_list.gridy = 0;
+		this.inputPanel.add(this.list, gbc_list);
+		
+		//A label that reads "Number of Clicks"
+		this.lblNumberOfClicks = new JLabel("Number of Clicks");
+		GridBagConstraints gbc_lblNumberOfClicks = new GridBagConstraints();
+		gbc_lblNumberOfClicks.insets = new Insets(0, 0, 5, 5);
+		gbc_lblNumberOfClicks.gridx = 0;
+		gbc_lblNumberOfClicks.gridy = 1;
+		this.inputPanel.add(lblNumberOfClicks, gbc_lblNumberOfClicks);
+		
+		//A label that reads "Delay (ms)"
+		this.lblDelay = new JLabel("Delay (ms)");
+		GridBagConstraints gbc_lblDelay = new GridBagConstraints();
+		gbc_lblDelay.insets = new Insets(0, 0, 5, 5);
+		gbc_lblDelay.gridx = 1;
+		gbc_lblDelay.gridy = 1;
+		this.inputPanel.add(this.lblDelay, gbc_lblDelay);
+		
+		//Create the button that begins the automated clicking.
+		this.btnNewButton = new JButton("Start Clicking");
+		GridBagConstraints gbc_btnNewButton = new GridBagConstraints();
+		gbc_btnNewButton.insets = new Insets(0, 0, 0, 5);
+		gbc_btnNewButton.gridx = 0;
+		gbc_btnNewButton.gridy = 3;
+		this.btnNewButton.addActionListener(new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				int numClick = Integer.parseInt(numberOfClicks.getText());
+				int numberOfClicks = Integer.parseInt(numClicks.getText());
+				int del = Integer.parseInt(delayTime.getText());
 				try {
-					Bot.clickALot(numClick);
+					Bot.clickALot(numberOfClicks, del);
 				} catch (AWTException e1) {
 					e1.printStackTrace();
+					System.out.println("... I have no idea what went wrong");
 				}
+				
 			}
-			
 		});
-		Container content = this.jframe.getContentPane();
-		content.add(this.numberOfClicks, BorderLayout.PAGE_START);
-		content.add(this.beginClicking, BorderLayout.PAGE_END);
+		this.inputPanel.add(this.btnNewButton, gbc_btnNewButton);
+		
+		//Create the button that runs the selected script.
+		btnRunScript = new JButton("Run Script");
+		GridBagConstraints gbc_btnRunScript = new GridBagConstraints();
+		gbc_btnRunScript.insets = new Insets(0, 0, 0, 5);
+		gbc_btnRunScript.gridx = 1;
+		gbc_btnRunScript.gridy = 3;
+		inputPanel.add(btnRunScript, gbc_btnRunScript);
+		
+				
 	}
 	
 	private void createAndShowGui() {
-		// The following three lines will be included in basically every GUI
-		// you see. If you don't include EXIT_ON_CLOSE, your application won't
-		// close properly!
 		jframe.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 		jframe.pack();
 		jframe.setVisible(true);
--- src/autoclicker/Bot.java
+++ src/autoclicker/Bot.java
@@ -10,21 +10,27 @@ public class Bot {
 	}
 	
 	
-	/*
-	 * Performs n clicks with the left mouse button.
+	/**
+	 * Performs n clicks with the left mouse button, with a delay of m ms (milliseconds) after each click
+	 * 
+	 * @param n
+	 * 			The number of clicks to be performed
+	 * @param m
+	 * 			The delay between each click
 	 */
-	public static void clickALot(int n) throws AWTException{
+	public static void clickALot(int n, int m) throws AWTException{
 		Robot r = new Robot();
 		int i = 0;
 		while (i < n){
 			r.mousePress(InputEvent.BUTTON1_DOWN_MASK);
 			r.mouseRelease(InputEvent.BUTTON1_DOWN_MASK);
+			r.delay(m);
 			i++;
 		}
 	}
 	
 	public static void main(String[] args) throws AWTException{
-		clickALot(100);
+		clickALot(100, 1);
 	}
 
 }
